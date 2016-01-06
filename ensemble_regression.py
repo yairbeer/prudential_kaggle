6b6c7853a3e311a80d0d074cba9fcb7f768f7595
Yair Beer <yaia1309@gmail.com>
2016-01-06 17:09:05 +0200

    preparing for ensemble

--- ensemble_regression.py
+++ ensemble_regression.py
@@ -1,9 +1,11 @@
+from sklearn.grid_search import ParameterGrid
 import pandas as pd
 import numpy as np
 import xgboostlib.xgboost as xgboost
 import glob
 from sklearn.linear_model import Lasso, LinearRegression
 from sklearn.cross_validation import StratifiedKFold
+import scipy.optimize as optimize
 
 __author__ = 'YBeer'
 
@@ -61,6 +63,7 @@ def quadratic_weighted_kappa(rater_a, rater_b, min_rating=None, max_rating=None)
     rater_a = np.array(rater_a, dtype=int)
     rater_b = np.array(rater_b, dtype=int)
     assert(len(rater_a) == len(rater_b))
+
     if min_rating is None:
         min_rating = min(min(rater_a), min(rater_b))
     if max_rating is None:
@@ -139,49 +142,72 @@ print train.shape[1], ' columns'
 # 4th
 # splitter = [2.46039684, 3.48430979, 4.30777339, 4.99072484, 5.59295844, 6.17412558, 6.79373477]
 # nelder mead opt
-splitter = np.array([2.66904789, 3.50581566, 4.2500559, 4.83546497, 5.64020492, 6.26927023, 6.8221132])
+splitter = np.array([2.46039684, 3.48430979, 4.30777339, 4.99072484, 5.59295844, 6.17412558, 6.79373477])
 riskless_splitter = np.array([1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5])
-best_metatrain = 0
+
+
+def opt_cut_v2_cv(x):
+    case = np.array(ranking(predicted_results, x)).astype('int')
+    score = -1 * quadratic_weighted_kappa(y_test, case, 1, 8)
+    # print score
+    return score
+
+best_risk = 0
+best_score = 0
 risk = 0.95
-final_splitter = list(splitter * risk + riskless_splitter * (1 - risk))
 
-# train machine learning
 regressor = LinearRegression(fit_intercept=True)
-# regressor = Lasso(fit_intercept=True)
+param_grid = [
+              {'risk': [0.95, 0.97, 0.98, 1, 1.03]}
+             ]
 
-# CV
-cv_n = 10
-kf = StratifiedKFold(train_result, n_folds=cv_n, shuffle=True)
+print 'start CV'
+for params in ParameterGrid(param_grid):
+    print params
 
-meta_train = np.ones((train.shape[0],))
-metric = []
-for train_index, test_index in kf:
-    X_train, X_test = train[train_index, :], train[test_index, :]
-    y_train, y_test = train_result[train_index].ravel(), train_result[test_index].ravel()
-    # train machine learning
+    # CV
+    cv_n = 12
+    kf = StratifiedKFold(train_result, n_folds=cv_n, shuffle=True)
 
-    regressor.fit(X_train, y_train)
+    metric = []
+    for train_index, test_index in kf:
+        X_train, X_test = train[train_index, :], train[test_index, :]
+        y_train, y_test = train_result[train_index].ravel(), train_result[test_index].ravel()
+        # train machine learning
 
-    # predict
-    predicted_results = regressor.predict(X_test)
-    meta_train[test_index] = predicted_results
-    classified_predicted_results = np.array(ranking(predicted_results, final_splitter)).astype('int')
-    predicted_results += 0.5
-    predicted_results = np.floor(predicted_results).astype('int')
-    predicted_results = predicted_results * (1 * predicted_results > 0) + 1 * (predicted_results < 1)
-    predicted_results = predicted_results * (1 * predicted_results < 9) + 8 * (predicted_results > 8)
-    print pd.Series(predicted_results).value_counts()
-    # print pd.Series(y_test).value_counts()
-    print quadratic_weighted_kappa(y_test, classified_predicted_results)
-    # print quadratic_weighted_kappa(y_test, predicted_results)
-    metric.append(quadratic_weighted_kappa(y_test, classified_predicted_results))
-print 'The quadratic weighted kappa is: ', np.mean(metric)
+        regressor.fit(X_train, y_train)
+
+        # predict
+        predicted_results = regressor.predict(X_test)
+        case = np.array(ranking(predicted_results, [2.46039684, 3.48430979, 4.30777339, 4.99072484, 5.59295844,
+                                                    6.17412558, 6.79373477])).astype('int')
+        # train machine learning
+        res = optimize.minimize(opt_cut_v2_cv, [2.46039684, 3.48430979, 4.30777339, 4.99072484, 5.59295844,
+                                                6.17412558, 6.79373477], method='Nelder-Mead',
+                                # options={'disp': True}
+                                )
+        # print res.x
+        cur_splitter = list(params['risk'] * res.x + (1 - params['risk']) * riskless_splitter)
+        # print cur_splitter
+        classified_predicted_results = np.array(ranking(predicted_results, cur_splitter)).astype('int')
+        # predict
+        print quadratic_weighted_kappa(y_test, classified_predicted_results, 1, 8)
+        metric.append(quadratic_weighted_kappa(y_test, classified_predicted_results, 1, 8))
+    print 'The quadratic weighted kappa is: ', np.mean(metric)
+    if np.mean(metric) > best_score:
+        print 'new best risk'
+        best_score = np.mean(metric)
+        best_risk = params['risk']
 
 regressor.fit(train, train_result)
+print 'The regression coefs are:'
 print regressor.coef_, regressor.intercept_
-
+print 'the best risk is: %f' % best_risk
+print 'the best quadratic weighted kappa is: %f' % best_score
 # predict
 predicted_results = regressor.predict(test)
+
+final_splitter = list(splitter * best_risk + riskless_splitter * (1 - best_risk))
 
 print 'writing to file'
 classed_results = np.array(ranking(predicted_results, final_splitter)).astype('int')
@@ -203,13 +229,14 @@ submission_file.to_csv("ensemble_linear_regression.csv")
 # added linear regression
 # only bossting regressor + linear, with intercept: 0.662427982667
 
-# added nedler mead optimized spliter
-# only bossting regressor + linear, with intercept: 0.666313598761
+# added nedler mead optimized spliter: 0.666313598761
 
-# added RF depth = 10
-# only bossting regressor + linear, with intercept: 0.663928238356
+# added RF depth = 10: 0.663928238356
+# added RF depth = 20: 0.664636681173
 
-# added RF depth = 20
-# only bossting regressor + linear, with intercept: 0.664636681173
+# added risk
 
-# risk = 0.95: 0.664498558395
+# added RF depth = 30, risk = 1: 0.668113299164
+
+# moved to n_CV = 12
+# risk = 1, QWK = 0.667689
